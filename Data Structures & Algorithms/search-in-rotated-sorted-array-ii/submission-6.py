9074bfbe1a23cb5053806311d5f28581101f2c42
Alberto Eusebio <72319445+AlbertoEusebio@users.noreply.github.com>
2026-04-02 23:46:33 +0200

    Add: search-in-rotated-sorted-array-ii - submission-6

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        
+        # find pivot then bs
+        l, r = 0, len(nums)-1
+
+        m = (l+r+1) // 2
+        while l <= r:
+            if nums[m] == target:
+                return True
+            if nums[l] < nums[m]:
+                # l --> m sorted
+                if target < nums[l] or nums[m] < target:
+                    l = m+1
+                else:
+                    r = m-1                
+            elif nums[l] == nums[m] == nums[r]:
+                r = r-1
+            else:
+                # m --> r sorted
+                if target < nums[m] or nums[r] < target:
+                    r = m-1
+                else:
+                    l = m+1
+         
+            m = (l+r+1) // 2
+            print(m)
+
+        return False
