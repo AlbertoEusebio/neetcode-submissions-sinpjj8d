ed54e03b591346279b54a154bdeda0778ee5e24a
Alberto Eusebio <72319445+AlbertoEusebio@users.noreply.github.com>
2026-04-09 08:57:44 +0200

    Add: mlp-from-scratch - submission-0

---
+++ Machine Learning/mlp-from-scratch/submission-0.py
@@ -0,0 +1,17 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import List
+
+
+class Solution:
+    def forward(self, x: NDArray[np.float64], weights: List[NDArray[np.float64]], biases: List[NDArray[np.float64]]) -> NDArray[np.float64]:
+        # x: 1D input array
+        # weights: list of 2D weight matrices
+        # biases: list of 1D bias vectors
+        # Apply ReLU after each hidden layer, no activation on output layer
+        # return np.round(your_answer, 5)
+        h=x
+        for W, b in zip(weights,biases):
+            z = h @ W + b
+            h = np.maximum(0, z)
+        return np.round(z, 5)
