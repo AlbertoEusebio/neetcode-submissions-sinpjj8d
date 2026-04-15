5705f6257a6a3aab100a08472785f9b1e77d147c
Alberto Eusebio <72319445+AlbertoEusebio@users.noreply.github.com>
2026-04-16 00:12:34 +0200

    Add: stone-game - submission-1

---
+++ Data Structures & Algorithms/stone-game/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        
+        tot = sum(piles)
+
+        memo = {}
+
+        def dfs(i, j, s, turn):
+
+            if i > j:
+                return s > 0
+
+            if (i, turn) in memo:
+                return memo[(i, turn)]
+
+            # take beginning or end
+
+            v = piles[i] * turn
+            u = piles[j] * turn
+
+            res = max(dfs(i+1, j, s+v, -1*turn), dfs(i, j-1, s+u, -1*turn))
+            memo[(i,turn)] = res
+            return res
+        return dfs(0, len(piles)-1, 0, 1)
